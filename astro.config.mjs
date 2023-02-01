2636c39955f26df3a8d9b847cbf7cb929fd92993
Didier Domínguez <dadu0699@hotmail.com>
2023-02-01 12:54:47 -0600

    feat: Migrating to Astro version 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-import tailwind from '@astrojs/tailwind';
+// https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: cloudflare({ mode: 'directory' }),
+  output: "server",
+  adapter: cloudflare(),
+  integrations: [tailwind()]
 });
