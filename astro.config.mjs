79ee2cb17216408f8a6506e8356a4b8cdabaadc6
Didier Domínguez <dadu0699@hotmail.com>
2023-02-01 13:11:06 -0600

    fix: Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import cloudflare from '@astrojs/cloudflare';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare(),
-  integrations: [tailwind()]
+  output: 'server',
+  adapter: cloudflare({ mode: 'directory' }),
+  integrations: [tailwind()],
 });
