dc1a73329b99b9ffcfdd6a659c593fd49cc982f5
Didier Domínguez <dadu0699@hotmail.com>
2023-11-20 15:36:13 -0600

    fix: Methods allowed by the endpoint

--- functions/api/generate-qr.js
+++ functions/api/generate-qr.js
@@ -6,7 +6,7 @@ export async function onRequestOptions() {
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers': 'Content-Type',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Methods': 'POST, OPTIONS',
       'Access-Control-Max-Age': '86400',
     },
   });
