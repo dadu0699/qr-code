17d6b34b51e8b257b0b6ad36efb5e678a24cb1a9
Didier Domínguez <dadu0699@hotmail.com>
2024-07-06 18:36:56 -0600

    build: Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({ mode: 'directory' }),
+  adapter: cloudflare(),
   integrations: [tailwind()],
 });
